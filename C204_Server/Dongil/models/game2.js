6f5f9e085c328208c9279c6a164f5874c58255b2
K-dongil <kdi1569@gmail.com>
2022-05-10 11:19:03 +0900

    feature: database update

--- C204_Server/Dongil/models/game2.js
+++ C204_Server/Dongil/models/game2.js
@@ -1,14 +1,8 @@
 const Sequelize = require('sequelize');
 
-module.exports = class Score extends Sequelize.Model {
+module.exports = class Game2 extends Sequelize.Model {
   static init(sequelize) {
     return super.init({
-      // init에 column들을 정의해 넣을 수 있다
-      game1: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        allowNull: true,
-        unique: false,
-      },
       game2: {
         type: Sequelize.INTEGER,
         allowNull: true,
@@ -20,8 +14,8 @@ module.exports = class Score extends Sequelize.Model {
       timestamps: false, // createdAt, updateAt 여부
       underscored: false, // sequelize에서 자동으로 만들어주는 것들에 대한 이름설정
       paranoid: false, // deleteAt(제거날짜) 여부, true일 때 soft delete
-      modelName: "Score", // 보통 단수형
-      tableName: "scores", // 모델이름의 복수형
+      modelName: "Game2", // 보통 단수형
+      tableName: "Game2", // 모델이름의 복수형
       charset: "utf8", // utf8mb4, utf8
       collate: "utf8_general_ci", // utf8mb4_general_ci, utf8_general_ci
     });
@@ -32,6 +26,6 @@ module.exports = class Score extends Sequelize.Model {
   // foreignKey가 targetKey를 참조할 것이다
   // belongsTo인 Table쪽에 foreignKey가 생성된다
   static associate(db) {
-    db.Score.belongsTo(db.User, { foreignKey: "id", targetKey: "id" });
+    db.Game2.belongsTo(db.User, { foreignKey: "user_id", targetKey: "user_id" });
   }
 }
