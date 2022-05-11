04c61a9a40abf1523a4fc1a9e1f2b56b814d3ef4
송상빈 <ssbin407@naver.com>
2022-05-11 11:15:17 +0900

    Fix: fix db table name

--- C204_Server/Dongil/models/game1.js
+++ C204_Server/Dongil/models/game1.js
@@ -15,8 +15,8 @@ module.exports = class Game1 extends Sequelize.Model {
       timestamps: false, // createdAt, updateAt 여부
       underscored: false, // sequelize에서 자동으로 만들어주는 것들에 대한 이름설정
       paranoid: false, // deleteAt(제거날짜) 여부, true일 때 soft delete
-      modelName: "Game1", // 보통 단수형
-      tableName: "Game1", // 모델이름의 복수형
+      modelName: "game1", // 보통 단수형
+      tableName: "game1", // 모델이름의 복수형
       charset: "utf8", // utf8mb4, utf8
       collate: "utf8_general_ci", // utf8mb4_general_ci, utf8_general_ci
     });
--- C204_Server/Dongil/models/game2.js
+++ C204_Server/Dongil/models/game2.js
@@ -14,8 +14,8 @@ module.exports = class Game2 extends Sequelize.Model {
       timestamps: false, // createdAt, updateAt 여부
       underscored: false, // sequelize에서 자동으로 만들어주는 것들에 대한 이름설정
       paranoid: false, // deleteAt(제거날짜) 여부, true일 때 soft delete
-      modelName: "Game2", // 보통 단수형
-      tableName: "Game2", // 모델이름의 복수형
+      modelName: "game2", // 보통 단수형
+      tableName: "game2", // 모델이름의 복수형
       charset: "utf8", // utf8mb4, utf8
       collate: "utf8_general_ci", // utf8mb4_general_ci, utf8_general_ci
     });
