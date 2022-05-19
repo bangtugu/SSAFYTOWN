d1e732a14806736315586b9ac0f1c0ecb27c0798
K-dongil <kdi1569@gmail.com>
2022-05-19 10:19:20 +0900

    fix: http method change

--- C204_Server/Dongil/routes/score/index.js
+++ C204_Server/Dongil/routes/score/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('./score.controller')
 
-router.post('/game1/:userId/:score', userController.game1);
-router.post('/game2/:userId/:score', userController.game2);
+router.post('/game1', userController.game1);
+router.post('/game2', userController.game2);
 
 module.exports = router;
